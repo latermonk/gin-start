f38e843651c3b3ecc7df857dbaae8dc110b73483
latermonk <iback.china@gmail.com>
2021-06-15 12:02:03 +0800

    0209

---
+++ 0209-obtain-body-parameter/go.mod
@@ -0,0 +1,5 @@
+module 0209-obtain-body-parameter
+
+go 1.16
+
+require github.com/gin-gonic/gin v1.7.2
---
+++ 0209-obtain-body-parameter/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bytes"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.POST("/test", func(c *gin.Context) {
+		bodyByts,err :=ioutil.ReadAll(c.Request.Body)
+		if err!= nil{
+			c.String(http.StatusBadRequest,err.Error())
+			c.Abort()
+		}
+		c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(bodyByts))
+
+		// 测试
+		firstName := c.PostForm("first_name")
+		lastName  := c.DefaultPostForm(
+			"last_name",
+			"last_default_name")
+
+		c.String(http.StatusOK, "%s,%s,%s", firstName,lastName, string(bodyByts))
+
+
+		//c.String(http.StatusOK, string(bodyByts))
+
+
+	})
+
+
+	r.Run(":9999")
+}
