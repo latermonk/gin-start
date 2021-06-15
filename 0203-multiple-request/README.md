f4a1f990fe216dddd4207ea95a53a9a193fcee5c
latermonk <iback.china@gmail.com>
2021-06-15 09:10:36 +0800

    0203

---
+++ 0203-multiple-request/README.md
@@ -0,0 +1,25 @@
+#  测试方法[POST  PUT  DELETE  GET]
+
+
+```api
+ curl -X GET "http://127.0.0.1:9999/get" 
+
+curl -X POST "http://127.0.0.1:9999/post"   
+
+curl -X DELETE  "http://127.0.0.1:9999/delete"   
+
+
+```
+
+
+#  Any方法
+
+```api
+curl -X POST  "http://127.0.0.1:9999/any"  
+curl -X PUT  "http://127.0.0.1:9999/any"  
+curl -X DELETE  "http://127.0.0.1:9999/any"
+curl -X GET  "http://127.0.0.1:9999/any"  
+
+```
+
+
--- 0203-multiple-request/main.go
+++ 0203-multiple-request/main.go
@@ -1,9 +1,26 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
 	r := gin.Default()
+
+	r.GET("/get", func(c *gin.Context) {
+		c.String(http.StatusOK, "get")
+	})
+	r.POST("/post", func(c *gin.Context) {
+		c.String(http.StatusOK, "post")
+	})
+	r.Handle("DELETE", "/delete", func(c *gin.Context) {
+		c.String(http.StatusOK, "delete")
+	})
+
+	r.Any("/any", func(c *gin.Context) {
+		c.String(http.StatusOK, "any")
+	})
 
 	r.Run(":9999")
 
